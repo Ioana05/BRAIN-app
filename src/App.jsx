208f5c86947996094c89c1c5568a9a9e617c9e8b
anarosu <anarosu766@gmail.com>
2025-09-07 17:08:55 +0300

    Add favicon and title for web tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import ArticleCarousel from "./components/ArticleCarousel";
 import Navbar from "./components/Navbar/Navbar";
 import Notifications from "./components/Notifications/Notifications";
@@ -10,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Navbar />}>
         <Route index element={<ArticleCarousel />} />
-        <Route path="/notifications" element={Notifications} />
+        <Route path="/notifications" element={<Notifications />} />
       </Route>
     </Routes>
   );
