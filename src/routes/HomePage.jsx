29cc3cc9a3ebbd2dd4ce4e5e7a5d98b74e787029
Leahu Silvia-Ioana <yoana.silvia2003@gmail.com>
2025-09-18 11:55:36 +0300

    changed manifest icon AGAIN

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,8 +1,19 @@
 import ArticleCarousel from "../components/ArticleCarousel";
 const HomePage = () => {
+  async function callTestEndpoint() {
+    try {
+      const response = await fetch("https://brain-app-two.vercel.app/api/test");
+      const data = await response.json();
+      console.log("Response from /api/test:", data);
+    } catch (err) {
+      console.error("Error fetching:", err);
+    }
+  }
+
   return (
     <div>
       <ArticleCarousel />
+      <button onClick={callTestEndpoint}>Test Notification</button>
     </div>
   );
 };
