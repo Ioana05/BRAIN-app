408a7f603c1b66a2b352b138c59479d06e1fc593
anarosu <anarosu766@gmail.com>
2025-09-18 14:06:05 +0300

    Remove test button

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,19 +1,8 @@
 import ArticleCarousel from "../components/ArticleCarousel";
 const HomePage = () => {
-  async function callTestEndpoint() {
-    try {
-      const response = await fetch("https://brain-app-two.vercel.app/api/test");
-      const data = await response.json();
-      console.log("Response from /api/test:", data);
-    } catch (err) {
-      console.error("Error fetching:", err);
-    }
-  }
-
   return (
     <div>
       <ArticleCarousel />
-      <button onClick={callTestEndpoint}>Test Notification</button>
     </div>
   );
 };
