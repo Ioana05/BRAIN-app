e29d601b555a02de3878f8300a430ef413d0de09
Leahu Silvia-Ioana <yoana.silvia2003@gmail.com>
2025-11-09 00:20:46 +0200

    Parsed ISO time into more readable time

--- src/components/NotificationCard/NotificationCard.jsx
+++ src/components/NotificationCard/NotificationCard.jsx
@@ -16,10 +16,24 @@ const NotificationCard = ({ title, message, time, isRead, onMarkAsRead }) => {
     window.open("https://www.edusoft.ro/anunturi.html", "_blank");
     onMarkAsRead();
   };
+
+  const parseData = (string) => {
+    const date = new Date(string);
+
+    const year = date.getUTCFullYear();
+    const month = (date.getUTCMonth() + 1).toString().padStart(2, "0"); // lunile încep de la 0
+    const day = date.getUTCDate().toString().padStart(2, "0");
+    const hours = date.getUTCHours().toString().padStart(2, "0");
+    const minutes = date.getUTCMinutes().toString().padStart(2, "0");
+
+    const formatted = `${day}-${month}-${year} ${hours}:${minutes}`;
+    return formatted;
+  };
+  const date = parseData(time);
   return isRead ? (
     <ReadCard>
       <div>
-        <Time>{time}</Time>
+        <Time>{date}</Time>
         <Title>{title}</Title>
         <Message>{message}</Message>
       </div>
@@ -30,7 +44,7 @@ const NotificationCard = ({ title, message, time, isRead, onMarkAsRead }) => {
   ) : (
     <UnreadCard>
       <div>
-        <Time>{time}</Time>
+        <Time>{date}</Time>
         <Title>{title}</Title>
         <Message>{message}</Message>
       </div>
