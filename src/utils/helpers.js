d450ed42e46495c1239dc26a3b42d85ca78f8f21
anarosu <anarosu766@gmail.com>
2025-11-15 23:25:43 +0200

    Display notification hour in local time

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,11 +1,11 @@
 export const parseData = (string) => {
   const date = new Date(string);
 
-  const year = date.getUTCFullYear();
-  const month = (date.getUTCMonth() + 1).toString().padStart(2, "0"); // lunile încep de la 0
-  const day = date.getUTCDate().toString().padStart(2, "0");
-  const hours = date.getUTCHours().toString().padStart(2, "0");
-  const minutes = date.getUTCMinutes().toString().padStart(2, "0");
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // months start at 0
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
 
   const formatted = `${day}-${month}-${year} ${hours}:${minutes}`;
   return formatted;
